b133cd549a5a59194f362aa333ea37c2c0b6dfa7
Daniel Angelus Gai Silva <danielangelusgai19@gmail.com>
2024-12-14 21:45:10 -0300

    Minor tweaks
    Just small changes

--- python/calculator.py
+++ python/calculator.py
@@ -87,7 +87,7 @@ btn_open = tk.Button(window, text = "(", command = lambda: add_to_calculation("(
 btn_open.grid(row = 5, column = 1)
 btn_close = tk.Button(window, text = ")", command = lambda: add_to_calculation(")"), width= 5, font = ("Arial", 14))
 btn_close.grid(row = 5, column = 3)
-#Removed lambda since we don't take any parameter, no () since we don't want to call, we want to pass it
+#Removed lambda since we don't take any parameter, no () since we don't want to call the function, we want to pass it
 btn_equal = tk.Button(window, text = "=", command = evaluate_calculation, width= 11, font = ("Arial", 14))
 btn_equal.grid(row = 6, column = 3, columnspan = 2)
 btn_clear = tk.Button(window, text = "C", command = clear_field, width= 11, font = ("Arial", 14))
--- python/numguesser.py
+++ python/numguesser.py
@@ -1,9 +1,14 @@
+#import random grants you access to a few functions in python
+#rand.int being one of them.
 import random
+
+#function to guess
 
 def guess (x):
     random_num = random.randint(1, x)
     guess = 0 
     while  (guess != random_num):
+        #let's you define the range
         guess = int (input (f"Guess a number between 1 and {x}: "))
         print(guess)
         
@@ -11,6 +16,8 @@ def guess (x):
             print("Guess again, too low.")
         elif guess > random_num:
             print("Guess again, too high.")
+    
     print(f"Congratulations, right guess. {random_num} was the right one.")
 
+#calling the function with a defined range
 guess(10)
